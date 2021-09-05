839d7c97d07dc5a371cd8606c774914c75e29f3d
mbuchet <mbuchet@student.s19.be>
2021-09-05 20:36:29 +0000

    pouet

---
+++ ex00/ft_putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    char lettre = 0;
+
+    scanf("%c", &lettre);
+    printf("%c\n", lettre);
+
+    return 0;
+}
+}
---
+++ ft_print_reverse_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void ft_print_alphabet(void);
+int main ()
+{
+        ft_print_alphabet();
+        return(0);
+}
+void ft_print_alphabet()
+{
+        char ch;
+        for(ch = 'z' ; ch <= 'a' ; ch++)
+        {
+                 putchar(ch);
+        }
+}
